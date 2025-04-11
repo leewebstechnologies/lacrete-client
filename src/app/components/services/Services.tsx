f568a45624567c70430b63a8be15891f99763ddc
Ali <aliuajibade317@gmail.com>
2025-04-12 02:12:50 +0400

    add services

--- src/app/components/services/Services.tsx
+++ src/app/components/services/Services.tsx
@@ -1,7 +1,6 @@
-
 import Link from "next/link";
 import styles from "./services.module.css";
-import {services} from "@/app/data";
+import { services } from "@/app/data";
 import Image from "next/image";
 
 const Services = () => {
@@ -12,23 +11,36 @@ const Services = () => {
       <h1 className={styles.subtitle}>Services we can help you with</h1>
       <div className={styles.services}>
         {services.map((item) => (
-
           <Link passHref key={item.id} href={`/products/${item.name}`}>
             <div className={styles.item}>
               <span className={styles.cat}>{item.title}</span>
               <div className={styles.media}>
-              {item.video ? (
-                <video controls src={item.video} autoPlay loop muted className={styles.video} />
-              ) : (
-                <Image src={item.photo} alt="photo" width={100} height={100} layout="responsive" className="photo" />
-              )}
-
+                {item.video ? (
+                  <video
+                    controls
+                    src={item.video}
+                    autoPlay
+                    loop
+                    muted
+                    className={styles.video}
+                  />
+                ) : (
+                  <Image
+                    src={item.photo}
+                    alt="photo"
+                    width={100}
+                    height={100}
+                    layout="responsive"
+                    objectFit="cover"
+                    className="photo"
+                  />
+                )}
               </div>
             </div>
           </Link>
         ))}
       </div>
     </div>
-  )
-}
-export default Services
+  );
+};
+export default Services;
